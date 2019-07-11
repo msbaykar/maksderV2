58113402f1097d24ee9012680762acd7d9d6b3a3
mehmet selim baykar <msbaykar@gmail.com>
2019-07-11 10:46:55 +0300

    son hali
    Signed-off-by: mehmet selim baykar <msbaykar@gmail.com>

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@ $(document).ready(function () {
     $('.formContainer').toggleClass('openForm');
  });
 });
+$(document).ready(function () {
+  $('.mobilKapat').on('click',function () {
+    $('.mobilMenuContainer').slideUp(500);
+    $('body').css('overflow','auto');
+
+
+
+  });
+  $('.mobilMenu button').on('click',function () {
+    $('.mobilMenuContainer').slideDown(500);
+    $('.mobilMenuContainer').css({
+      'display': 'flex',
+
+    });
+
+    $('body').css('overflow','hidden');
+  })
+
+})
 // owl slider
 $('.mainSlider').owlCarousel({
   loop:true,
@@ -24,4 +43,42 @@ $('.mainSlider').owlCarousel({
       loop:true
     }
   }
-})
+});
+$('.userSlider').owlCarousel({
+  loop:true,
+  margin:10,
+  autoplay:true,
+  responsiveClass:true,
+  responsive:{
+    0:{
+      items:2,
+      nav:true
+    },
+    600:{
+      items:4,
+      nav:false
+    },
+    1000:{
+      items:6,
+      nav:false,
+      loop:true
+    }
+  }
+});
+
+
+
+//
+// // init currency loader with the api address
+// // that fetches xml data from http://www.tcmb.gov.tr/kurlar/today.xml
+// load_tcmb_currencies('/examples/api/tcmb_currency.php');
+// // since AUD is not added by default
+// var currencies = {'USD':'$','EUR':'€','GBP':'£','AUD':'$'};
+// // ask to load currencies into the given element by a selector
+// load_exchange_table('#exchange',currencies);
+// // since AUD is not added by default
+// var currencies = ['USD','EUR','GBP','AUD'];
+//
+// // ask to load currencies into the given element by a selector
+// load_converter_form('#converter',currencies);
+
